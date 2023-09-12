b489f63845386e631bd90646b76b591b25333b76
desmond <afaridesmond@gmail.com>
2023-09-12 07:29:09 +0000

    method to create new user updated

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -31,7 +31,7 @@ class DB:
         return self.__session
 
     def add_user(self, email: str, hashed_password: str) -> User:
-        "method to add a new user"
+        """method to add a new user"""
         new_user = User(email=email, hashed_password=hashed_password)
         self._session.add(new_user)
         self._session.commit()
